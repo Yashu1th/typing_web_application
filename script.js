3b53a467d9027f3e135ccdbd55f18aaae275fe78
yaswanth narikamalli <yaswanthnarikamali@gmail.com>
2023-06-13 19:02:44 +0530

    working on js

---
+++ script.js
@@ -0,0 +1,10 @@
+// let typingSpeed=(actualWords/totalTimeTaken)*60;
+
+const typing_ground =document.getElementById('textarea');
+const btn =document.getElementById('btn');
+const score =document.getElementById('score');
+const show_sentence=document.getElementById('showSentance');
+
+
+
+let startTime, endTime, totalTimeTaken;
